532f06c45b16c779ea01fb68ed0a174f09ecda1d
yirmiri <yirmirilucy@gmail.com>
2024-04-08 01:36:22 -0400

    Balance changes

--- src/main/java/net/azurune/dried_spice/block/WildCoffeeBushBlock.java
+++ src/main/java/net/azurune/dried_spice/block/WildCoffeeBushBlock.java
@@ -5,13 +5,17 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class WildCoffeeBushBlock extends CoffeeBushBlock {
+    private static final VoxelShape SAPLING_SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
+    private static final VoxelShape MID_GROWTH_SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 8.0D, 13.0D);
+
     public WildCoffeeBushBlock(Properties properties) {
         super(properties);
-        registerDefaultState(defaultBlockState().setValue(AGE, 3));
     }
 
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
